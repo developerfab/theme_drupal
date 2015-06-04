808b40f70ceba892fd44bc143a6d4dd061d4ed37
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2015-06-04 06:04:34 -0500

    se cambia el codigo de la plantilla base en el theme

--- page.tpl.php
+++ page.tpl.php
@@ -1,148 +1,32 @@
 <?php require_once("header.tpl.php"); ?>
-<style type="text/css">
-.titulo{
-  font: caption;
-  font-weight: bold; 
-  color: #C13742;
+<script type="text/javascript">
+function efecto_cambio(tag_id, nombre_img){
+  var ruta = "<?php print base_path() . path_to_theme(); ?>/images/"+nombre_img;
+  document.getElementById(tag_id).src=ruta;
 }
-</style>
-<center>
-  <table width="100%" cellspacing="0" cellpadding="0" style="height:100% ">
-    <tr align="left" valign="top">
-      <td style="background-position:top; background-repeat:repeat-x ">&nbsp;</td>
-      <td width="760" height="779" style="background-image:url(<?php print base_path() . path_to_theme(); ?>/images/BGCenter100.jpg) "><table width="635" cellspacing="0" cellpadding="0" style="margin-left:62px; margin-top:71px ">
-        <tr>
-          <td height="104" align="left" valign="top"><table width="100%"   cellspacing="0" cellpadding="0">
-            <tr valign="top">
-              <td width="205"><table width="100%"   cellspacing="0" cellpadding="0">
-                <tr>
-                  <td height="75" align="right" valign="top"><img alt="" src="<?php print $logo; ?>" width="197" height="61"></td>
-                </tr>
-                <tr>
-                  <!-- COLUMNA IZQUIERDA -->
-                  <td align="right" valign="top"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Ourproducts.jpg" width="171" height="30" style="margin-right:12px "><br>
-                    <?php print render( $page['col_left'] ); ?>
-                  </td>
-                </tr>
-              </table></td>
-              <td><a href="index.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image1','','<?php print base_path() . path_to_theme(); ?>/images/Home+.jpg',0)"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Homa-.jpg" name="Image1" width="106" height="104" ></a><a href="index-1.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image2','','<?php print base_path() . path_to_theme(); ?>/images/Aboutus+.jpg',1)"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/About-.jpg" name="Image2" width="105" height="104" ></a><a href="index-2.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image3','','<?php print base_path() . path_to_theme(); ?>/images/Services+.jpg',0)"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Services-.jpg" name="Image3" width="105" height="104" ></a><a href="index-3.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image4','','<?php print base_path() . path_to_theme(); ?>/images/Contact+.jpg',1)"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Contact-.jpg" name="Image4" width="108" height="104" ></a></td>
-            </tr>
-          </table></td>
-        </tr>
-        <tr>
-          <td height="603" align="left" valign="top"><table width="100%"   cellspacing="0" cellpadding="0">
-            <tr>
-              <td width="22" valign="top">&nbsp;</td>
-              <td width="171" valign="top"><table width="100%"   cellspacing="0" cellpadding="0">
-                <tr>
-                  <td height="127" align="left" valign="top"><div style="width:100%; height:127x; overflow:auto ">
-                      <table width="100%"   cellspacing="0" cellpadding="0">
-                        <tr>
-                          <td width="22">&nbsp;</td>
-                          <!--td><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="15" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Lorem ipsum</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Amet consetetur </a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Sadipscing </a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Diam nomy</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Magna aliquyam</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Erat sed</a></td-->
-                        </tr>
-                      </table>
-                  </div></td>
-                </tr>
-                <tr>
-                  <td height="45" align="left" valign="top"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Ourtechnologies.jpg" width="171" height="30"></td>
-                </tr>
-                <tr>
-                  <td height="120" align="left" valign="top"><div style="width:100%; height:130px; overflow:auto ">
-                      <table width="100%"   cellspacing="0" cellpadding="0">
-                        <tr>
-                          <td width="22">&nbsp;</td>
-                          <!--td><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="15" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Magna aliquy </a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Erat sed</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Amet consetetur</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Voluptua sed </a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Ipsum dolorem</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Lorem ipsen</a><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="3" style="display:block "><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Marker100.jpg" width="6" height="6" style="margin-right:7px "><a href="#" class="usial">Sadipscing</a></td-->
-                        </tr>
-                      </table>
-                  </div></td>
-                </tr>
-              </table></td>
-              <td width="19">&nbsp;</td>
-              <!-- COLUMNA CENTRAL DEL CONTENIDO-->
-              <td width="403" valign="top"><table width="100%"   cellspacing="0" cellpadding="0">
-                <tr>
-                  <td height="152" align="left" valign="top"><table width="382"   cellspacing="0" cellpadding="0" style="margin-left:12px; margin-top:25px ">
-                    <tr>
-                      <td height="33" align="left" valign="top"><!--img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Wellcome.jpg" width="382" height="18"--><span class="titulo"><?php print $site_name; ?></span></td>
-                    </tr>
-                    <tr>
-                      <td align="left" valign="top">
-                        <div style="width:100%; height:70px; overflow:auto ">
-                          <table width="100%"   cellpadding="0" cellspacing="0" class="usial">
-                            <tr align="left" valign="top">
-                              <td width="110" rowspan="2"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Foto100.jpg" width="97" height="56"></td>
-                              <td height="40">
-                                <p><?php print render( $page['content'] ); ?></p>
-                              </td>
-                            </tr>
-                            <tr align="left" valign="top">
-                              <td align="right"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/marker200.jpg" width="9" height="3" style="margin-right:3px "><a href="#" class="usial2"><strong>read more</strong></a></td>
-                            </tr>
-                          </table>
-                        </div>
-                      </div></td>
-                    </tr>
-                  </table></td>
-                </tr>
-                <!--tr>
-                  <td align="left" valign="top"><table width="100%"   cellspacing="0" cellpadding="0">
-                    <tr align="left" valign="top">
-                      <td width="212"><table width="100%"   cellspacing="0" cellpadding="0">
-                        <tr>
-                          <td height="46" align="left" valign="top"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Capabilites.jpg" width="212" height="30"></td>
-                        </tr>
-                        <tr>
-                          <td height="90" align="right" valign="top"><div style="width:100%; height:100px; overflow:auto ">
-                            <table width="203"   cellpadding="0" cellspacing="0" class="usial2">
-                              <tr align="left" valign="top">
-                                <td width="83" rowspan="2"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Foto200.jpg" width="78" height="72"></td>
-                                <td><strong>Lorem ipsum dolor</strong><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="5" style="display:block ">Amet consetetur dolore, sadipscing elitr sit sed nonumy set.</td>
-                              </tr>
-                              <tr align="left" valign="top">
-                                <td align="right"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/marker200.jpg" width="9" height="3" style="margin-right:3px "><a href="#" class="usial2"><strong>read more</strong></a></td>
-                              </tr>
-                            </table>
-                          </div></td>
-                        </tr>
-                      </table></td>
-                      <td>&nbsp;</td>
-                      <td width="172"><table width="100%"   cellspacing="0" cellpadding="0">
-                        <tr>
-                          <td height="46" align="left" valign="top"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/FullColor.jpg" width="172" height="30"></td>
-                        </tr>
-                        <tr>
-                          <td height="130" align="right" valign="top"><table width="160"   cellspacing="0" cellpadding="0">
-                            <tr>
-                              <td height="100" valign="top" class="usial2"><strong>Lorem ipsum dolor sit amet, </strong>sadipscing elitr sed diame nonumy eirmod tempor invidunt ut labore aliquyam erat sed voluptua.<img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/spacer.gif" height="15" style="display:block "><strong>Sadipscing elitr sed</strong>, diam nony eirmod tempor invidunt.</td>
-                            </tr>
-                            <tr>
-                              <td align="right" valign="top" class="usial2"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/marker200.jpg" width="9" height="3" style="margin-right:3px "><a href="#" class="usial2"><strong>read more</strong></a></td>
-                            </tr>
-                          </table></td>
-                        </tr>
-                        <tr>
-                          <td height="118" align="right" valign="top"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/banner1.jpg" width="172" height="90"></td>
-                        </tr>
-                        <tr>
-                          <td align="right" valign="top" class="CR">&copy; Copyright 2006. All rights reserved</td>
-                        </tr>
-                        <tr>
-                          <td align="right" valign="top"></td>
-                        </tr>
-                      </table></td>
-                    </tr>
-                  </table></td>
-                </tr-->
-              </table></td>
-              <td>&nbsp;</td>
-            </tr>
-          </table></td>
-        </tr>
-      </table></td>
-      <td style="background-position:top; background-repeat:repeat-x ">&nbsp;</td>
-    </tr>
-    
-  </table>
-</center>
+</script>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<div class="container contenedor">
+    <nav class="col-md-offset-2 col-md-10">
+      <div class="logo"><img alt="" src="<?php print $logo; ?>"></div>
+      <div class="menu"><a href="." onMouseOut="efecto_cambio('Image1','Homa-.jpg');" onMouseOver="efecto_cambio('Image1','Home+.jpg');"><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Homa-.jpg" id="Image1"></a></div>
+      <div class="menu"><a href="." onMouseOut="efecto_cambio('Image2','About-.jpg');" onMouseOver="efecto_cambio('Image2','Aboutus+.jpg');"><img alt="" src="http://localhost/internet/themes/fotocopiadora/images/About-.jpg" id="Image2"></a></div>
+      <div class="menu"><a href="." onMouseOut="efecto_cambio('Image3','Services-.jpg');" onMouseOver="efecto_cambio('Image3','Services+.jpg');"><img alt="" src="http://localhost/internet/themes/fotocopiadora/images/Services-.jpg" id="Image3"></a></div>
+      <div class="menu"><a href="." onMouseOut="efecto_cambio('Image4','Contact-.jpg');" onMouseOver="efecto_cambio('Image4','Contact+.jpg');"><img alt="" src="http://localhost/internet/themes/fotocopiadora/images/Contact-.jpg" id="Image4"></a></div>
+    </nav>
+    <div class="col-md-offset-2">
+      <div class="col-der">
+        <ul>
+          <li><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Ourproducts.jpg" width="171" height="30" style="margin-right:12px "></li>
+        </ul>
+        <ul>
+          <?php print render( $page['col_left'] ); ?>
+        </ul>
+        <ul>
+          <li><img alt="" src="<?php print base_path() . path_to_theme(); ?>/images/Ourtechnologies.jpg" width="171" height="30"></li>
+        </ul>
+      </div>
+      <div class="col-contenido"><p><?php print render( $page['content'] ); ?></p></div>
+    </div>
+  </div>
 <?php require_once("footer.tpl.php"); ?>
